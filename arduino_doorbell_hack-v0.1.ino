cabff933374636f05f2cabfa426e3d388cd46de6
Danny Tucek <131976778+danitucek@users.noreply.github.com>
2025-06-10 23:06:14 +0200

    Update arduino_doorbell_hack-v0.2.ino
    added a function for an LED to indicate the isPlaying() state

--- arduino_doorbell_hack-v0.1.ino
+++ arduino_doorbell_hack-v0.1.ino
@@ -11,24 +11,33 @@ TMRpcm player;
 int button = 2;
 int volume = 5; //set the volume outside the code
 int buttonState;
+int led = 5;
+
+void updateLed() {
+  bool playing = player.isPlaying();
+  if (playing == true) {
+    digitalWrite(led, HIGH);
+  } else {
+    digitalWrite(led, LOW);
+  }
+}
 
 void setup() {
   pinMode(button, INPUT_PULLUP);
+  pinMode(led, OUTPUT);
   player.speakerPin = 9;
   Serial.begin(9600);
   if (!SD.begin(SD_ChipSelectPin)) {
-  Serial.println("SD fail");
-  return;
+    Serial.println("SD fail");
+    return;
   }
-
   player.setVolume(volume);
 }
+
 void loop() {
+  updateLed();
   buttonState = digitalRead(button);
-  Serial.print("Button status: ");
-  Serial.println(buttonState);
   if (buttonState == 0) {
     player.play("dingdong.wav");
-    player.isPlaying();
   }
 }
